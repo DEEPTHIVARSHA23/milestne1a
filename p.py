746603a18d852e87f57399a47e15d0142f317818
Deepthivarsha E G <89890778+DEEPTHIVARSHA23@users.noreply.github.com>
2022-03-08 13:49:39 +0530

    Update p.py

--- p.py
+++ p.py
@@ -5,3 +5,15 @@ with open(r'C:\Users\Hp\Downloads\DataSet.zip\Milestone1\Milestone1A.yaml') as f
 
 for item,doc in documents.items():
     print(item,".",doc)
+import os
+import yaml
+import logging.config
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('This is a log message.')
+
+import datetime
+now = datetime.datetime.now()
+print (now.strftime("%Y-%m-%d %H:%M:%S"),end="")
+print("000000")
